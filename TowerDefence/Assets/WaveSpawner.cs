8688233f07b7cc30113bae7493a20aee77a95730
hanleydev <31200813+hanleydev@users.noreply.github.com>
2017-08-23 22:13:44 +1000

    Waypoints, WaveSpawner, Enemys
    - Start and End points for WaveSpawner added
    - Ground Plane added
    - Camera rotated and adjusted
    - Waypoints created
    - Fixed Pathing positions as found wasnt correct
    - Enemy Prefab created
    - Removed 2x redundant nodes
    - GameManager script added
    - WaveSpawner Basic script added and tested
    - Basic UI Element for Countdown timer for WaveSpawner added

---
+++ TowerDefence/Assets/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    public float enemyDelay = 0.5f;
+    private float countdown = 2f;
+    private int waveIndex = 0;
+
+    public Text waveCountdownText;
+
+    void Update()
+    {
+        if (countdown <= 0f) {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    //TODO: Watch WaveSpawner MiniSeries - Brackeys
+    IEnumerator SpawnWave() {
+        Debug.Log("Wave Incoming");
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(enemyDelay);
+        }
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
